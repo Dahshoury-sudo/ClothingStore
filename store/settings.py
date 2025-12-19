042bf54b0f1b66cdd77abe40dfd93880ea2aa1fb
Mohamed Fouad <mfb4010@gmail.com>
2025-12-19 14:20:10 +0200

    removed the commented shity

--- store/settings.py
+++ store/settings.py
@@ -89,16 +89,6 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('db_name'),
-#         'USER': os.environ.get('db_user'),
-#         'PASSWORD': os.environ.get('db_password'),
-#         'HOST': os.environ.get('db_host'),
-#         'PORT': os.environ.get('db_port')
-#     }
-# }
 
 DATABASES = {
 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
