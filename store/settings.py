3e1fde6855fe13fe3990d27645c2535956bdfb58
Mohamed Fouad <mfb4010@gmail.com>
2025-12-19 14:33:43 +0200

    idk

--- store/settings.py
+++ store/settings.py
@@ -85,12 +85,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'store.wsgi.application'
 
-DATABASES = {
-'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+
+DATABASES = {
+'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 # DATABASES = {
 #     'default': {
